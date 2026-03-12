2bdd044cd5700b15213e184d8b7f3a39cedd2e60
Bond322 <ngabayiulricch04@gmail.com>
2026-03-12 23:14:21 +0300

    ssignment.py

---
+++ assignment.py
@@ -0,0 +1,25 @@
+# Write a Python program that prints the following message:
+print ("Hello AI Students")
+
+# Create the following variables in Python:
+name = "landry"
+age = 30
+city = "Nairobi"
+print ( f" Name: {name}") 
+print ( f" Age : {age}")
+print (f" City: {city}")
+
+# Create a list of five programming languages.
+list_programm = ["Python", "Java", "C++", "JavaScript", "Go"]
+print (list_programm [0])
+
+# Create the dictionary
+student = {
+"Name": "Landry",
+"age" : 30, 
+"course" : "AI"
+}
+# Print the student's name
+print ( student ["Name"])
+
+
